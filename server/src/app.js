7a5690b20db417739d3a0adc68551c5f2680b2a6
PavaniGudupu <pavani9419@gmail.com>
2026-06-21 17:01:03 +0530

    feat: add JSON request body parsing

--- server/src/app.js
+++ server/src/app.js
@@ -3,11 +3,19 @@ const loggerMiddleware = require("./middleware/loggerMiddleware");
 
 const app = express();
 
+app.use(express.json());
+
 app.use(loggerMiddleware);
 
 app.get("/", (req, res) => {
   res.json({
     message: "Portfolio Management System API Running",
+  });
+});
+
+app.post("/test", (req, res) => {
+  res.json({
+    receivedData: req.body,
   });
 });
 
