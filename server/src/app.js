c734ee529066a66671e796d19bb7cfeb2232524c
PavaniGudupu <pavani9419@gmail.com>
2026-06-21 16:53:01 +0530

    feat: add request logging middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const loggerMiddleware = require("./middleware/loggerMiddleware");
 
 const app = express();
+
+app.use(loggerMiddleware);
 
 app.get("/", (req, res) => {
   res.json({
